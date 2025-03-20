bbf1ed84a7ebf0ef8c3970b104de1ddf3aabdb9b
maarcoos8 <maarcoos_8@uma.es>
2025-03-20 11:30:22 +0100

    Creación proyecto

---
+++ src/main/java/org/mps/tree/BinarySearchTreeException.java
@@ -0,0 +1,7 @@
+package src.main.java.org.mps.tree;
+
+public class BinarySearchTreeException extends RuntimeException {
+    public BinarySearchTreeException(String message) {
+        super(message);
+    }
+}
